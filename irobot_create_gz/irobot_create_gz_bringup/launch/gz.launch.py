b96abceb64cb619b704d1a968f34f04ced80f429
Federico Ciresola <cirefede@gmail.com>
2024-04-17 15:35:47 +0200

    feat: added ifcondition to choose if the user want to use the HMI GUI on gazebo or not

--- irobot_create_gz/irobot_create_gz_bringup/launch/gz.launch.py
+++ irobot_create_gz/irobot_create_gz_bringup/launch/gz.launch.py
@@ -8,8 +8,8 @@ from pathlib import Path
 from ament_index_python.packages import get_package_share_directory
 
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument
-from launch.actions import IncludeLaunchDescription, SetEnvironmentVariable, AppendEnvironmentVariable
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription, AppendEnvironmentVariable
+from launch.conditions import IfCondition, UnlessCondition
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
 from launch_ros.actions import Node
@@ -19,8 +19,10 @@ ARGUMENTS = [
     DeclareLaunchArgument('use_sim_time', default_value='true',
                           choices=['true', 'false'],
                           description='use_sim_time'),
-    DeclareLaunchArgument('world', default_value='depot',
+    DeclareLaunchArgument('world', default_value='maze',
                           description='Gz World'),
+    DeclareLaunchArgument('gui', default_value='false',
+                            description='Enable gui plugin for create3')
 ]
 
 
@@ -55,6 +57,19 @@ def generate_launch_description():
         [pkg_ros_gz_gazebo, 'launch', 'gz_sim.launch.py'])
 
     # gz gazebo
+    gz_gazebo_gui = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([gz_gazebo_launch]),
+        launch_arguments=[
+            ('gz_args', [LaunchConfiguration('world'),
+                          '.sdf',
+                          ' -v 4',
+                          ' --gui-config ',
+                          PathJoinSubstitution([pkg_irobot_create_gz_bringup, 'gui', 'create3', 'gui.config'])
+                        ])
+        ],
+        condition=IfCondition(LaunchConfiguration('gui'))
+    )
+
     gz_gazebo = IncludeLaunchDescription(
         PythonLaunchDescriptionSource([gz_gazebo_launch]),
         launch_arguments=[
@@ -62,11 +77,11 @@ def generate_launch_description():
                           '.sdf',
                           ' -v 4',
                           ' --gui-config ',
-#                          PathJoinSubstitution([pkg_irobot_create_gz_bringup,
-#                                                'gui', 'create3', 'gui.config'])
                         ])
-        ]
+        ],
+        condition=UnlessCondition(LaunchConfiguration('gui'))
     )
+
 
     # clock bridge
     clock_bridge = Node(package='ros_gz_bridge', executable='parameter_bridge',
@@ -80,6 +95,7 @@ def generate_launch_description():
     ld = LaunchDescription(ARGUMENTS)
     ld.add_action(gz_resource_path)
     ld.add_action(gz_gui_plugin_path)
+    ld.add_action(gz_gazebo_gui)
     ld.add_action(gz_gazebo)
     ld.add_action(clock_bridge)
     return ld
